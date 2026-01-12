dd473f556b28914a31926f832909f4526ec8117c
Piyush Singh <workspace.piyush01@gmail.com>
2026-01-12 17:10:29 +0530

    Entities created, mapping implemented

---
+++ src/main/java/com/RESTapis/javaBackend/entity/User.java
@@ -0,0 +1,35 @@
+package com.RESTapis.javaBackend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User extends BaseEntity {
+
+    @NotBlank
+    @Column(nullable = false)
+    private String username;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    // getters & setters
+}
